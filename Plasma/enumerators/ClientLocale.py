c151bac465d2d8c1846f965a7c8f6c2b8e085b4a
Marek Grzyb <grzybdev@gmail.com>
2022-11-23 21:35:37 +0100

    Added ClientLocale enumerator

---
+++ Plasma/enumerators/ClientLocale.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class ClientLocale(Enum):
+    English = "en_US"
+    French = "fr_FR"
+    Spanish = "es"
+    German = "de"
+    Italian = "it"
+    Polish = "pl"
+    Russian = "ru"
+    Japanese = "ja"
