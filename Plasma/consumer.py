171ae64849d8dd6cf6f51fb69d3a2d5e8bc7ff36
Marek Grzyb <grzybdev@gmail.com>
2022-11-23 21:37:42 +0100

    Added ConnectService

--- Plasma/consumer.py
+++ Plasma/consumer.py
@@ -1,17 +1,55 @@
-from BFBC2_MasterServer.consumer import BFBC2Consumer
+import threading
 
-from Plasma.transactor import TransactionKind, Transactor
+from BFBC2_MasterServer.consumer import BFBC2Consumer
+from BFBC2_MasterServer.globals import (
+    CLIENT_INITIAL_MEMCHECK_INTERVAL,
+    CLIENT_MEMCHECK_INTERVAL,
+    CLIENT_PING_INTERVAL,
+    SERVER_INITIAL_MEMCHECK_INTERVAL,
+    SERVER_MEMCHECK_INTERVAL,
+    SERVER_PING_INTERVAL,
+)
+from packaging import version
+
+from Plasma.enumerators.ClientLocale import ClientLocale
+from Plasma.enumerators.ClientPlatform import ClientPlatform
+from Plasma.enumerators.ClientType import ClientType
+from Plasma.services.connect import TXN as ConnectTXN
+from Plasma.transactor import TransactionKind, TransactionService, Transactor
 
 
 class PlasmaConsumer(BFBC2Consumer):
 
+    clientString: str = None
+    clientPlatform: ClientPlatform = None
+    clientVersion = None
+    clientType: ClientType = None
+    sku: str = None
+    locale: ClientLocale = None
+    SDKVersion = None
+    protocolVersion = None
+    fragmentSize: int = None
     initialized = False
 
     transactor = None
 
+    memcheckTimer = None
+    pingTimer = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.transactor = Transactor(self)
+
+    async def disconnect(self, code):
+        await super().disconnect(code)
+
+        if self.pingTimer is not None:
+            self.pingTimer.cancel()
+            self.pingTimer = None
+
+        if self.memcheckTimer is not None:
+            self.memcheckTimer.cancel()
+            self.memcheckTimer = None
 
     async def receive(self, text_data=None, bytes_data=None):
         message = await super().receive(text_data, bytes_data)
@@ -26,3 +64,98 @@ class PlasmaConsumer(BFBC2Consumer):
             packet.kind = packet.kind & 0xFF000000 | tid
 
         await super().send_packet(packet)
+
+    async def initialize_connection(self, data: dict):
+        self.clientString = data["clientString"]
+        self.clientPlatform = ClientPlatform(data["clientPlatform"])
+        self.clientVersion = version.parse(data["clientVersion"])
+        self.clientType = ClientType(data["clientType"])
+        self.sku = data["sku"]
+        self.locale = ClientLocale(data["locale"])
+        self.SDKVersion = version.parse(data["SDKVersion"])
+        self.protocolVersion = version.parse(data["protocolVersion"])
+        self.fragmentSize = data["fragmentSize"]
+        self.initialized = True
+
+        self.logger.info(
+            f"Connection with {self.clientString} [{self.clientType}] (Platform: {self.clientPlatform}, Locale: {self.locale}, Version: {self.clientVersion}) initialized"
+        )
+
+        await self.send(
+            text_data=f"Hello {self.clientString}! You are now connected to Plasma Master Server emulator (as {'client' if self.clientType == ClientType.CLIENT else 'server'}). Have fun!"
+        )
+
+        if self.clientVersion != version.parse("1.0"):
+            self.logger.warning(
+                f"Client version {self.clientVersion} is not officially supported by this server emulator!"
+            )
+
+            await self.send(
+                text_data=f"WARNING: Your client version ({self.clientVersion}) is not officially supported by this server emulator, some features may not work properly. Please install latest supported game version by this server emulator (Which is 795745)"
+            )
+
+        if self.SDKVersion != version.parse("5.1.2.0.0"):
+            self.logger.warning(
+                f"SDK version {self.SDKVersion} is not officially supported by this server emulator!"
+            )
+
+            await self.send(
+                text_data=f"WARNING: SDK version ({self.clientVersion}) is not officially supported by this server emulator, some features may not work properly. Please install latest supported game version by this server emulator (Which is 795745)"
+            )
+
+        if self.protocolVersion != version.parse("2.0"):
+            self.logger.warning(
+                f"Protocol version {self.protocolVersion} is not officially supported by this server emulator!"
+            )
+
+            await self.send(
+                text_data=f"WARNING: Protocol version ({self.clientVersion}) is not officially supported by this server emulator, some features may not work properly. Please install latest supported game version by this server emulator (Which is 795745)"
+            )
+
+        await self.transactor.start(
+            TransactionService.ConnectService, ConnectTXN.MemCheck, {}
+        )
+
+        if self.memcheckTimer is None and self.pingTimer is None:
+            # Activate both ping and memcheck timers
+            self.pingTimer = threading.Timer(
+                CLIENT_PING_INTERVAL
+                if self.clientType == ClientType.CLIENT
+                else SERVER_PING_INTERVAL,
+                self.__ping_client,
+            )
+            self.memcheckTimer = threading.Timer(
+                CLIENT_INITIAL_MEMCHECK_INTERVAL
+                if self.clientType == ClientType.CLIENT
+                else SERVER_INITIAL_MEMCHECK_INTERVAL,
+                self.__memcheck_client,
+            )
+
+            self.pingTimer.start()
+            self.memcheckTimer.start()
+
+    async def __memcheck_client(self):
+        await self.transactor.start(
+            TransactionService.ConnectService, ConnectTXN.MemCheck, {}
+        )
+
+        self.memcheckTimer = threading.Timer(
+            CLIENT_MEMCHECK_INTERVAL
+            if self.clientType == ClientType.CLIENT
+            else SERVER_MEMCHECK_INTERVAL,
+            self.__memcheck_client,
+        )
+        self.memcheckTimer.start()
+
+    async def __ping_client(self):
+        await self.transactor.start(
+            TransactionService.ConnectService, ConnectTXN.Ping, {}
+        )
+
+        self.pingTimer = threading.Timer(
+            CLIENT_PING_INTERVAL
+            if self.clientType == ClientType.CLIENT
+            else SERVER_PING_INTERVAL,
+            self.__ping_client,
+        )
+        self.pingTimer.start()
---
+++ Plasma/services/connect.py
@@ -0,0 +1,190 @@
+import random
+import string
+from datetime import datetime
+from enum import Enum
+
+from BFBC2_MasterServer.globals import (
+    MESSENGER_IP,
+    MESSENGER_PORT,
+    THEATER_CLIENT_IP,
+    THEATER_CLIENT_PORT,
+    THEATER_SERVER_IP,
+    THEATER_SERVER_PORT,
+)
+from BFBC2_MasterServer.packet import Packet
+from BFBC2_MasterServer.service import Service
+from Plasma.enumerators.ClientType import ClientType
+from Plasma.error import TransactionSkip
+
+
+class TXN(Enum):
+    Hello = "Hello"
+    MemCheck = "MemCheck"
+    Ping = "Ping"
+    GetPingSites = "GetPingSites"
+    Goodbye = "Goodbye"
+    Suicide = "Suicide"
+
+
+class ConnectService(Service):
+    def __init__(self, connection) -> None:
+        super().__init__(connection)
+
+        self.resolver_map[TXN.Hello] = self.__handle_hello
+        self.creator_map[TXN.MemCheck] = self.__create_memcheck
+        self.creator_map[TXN.Ping] = self.__create_ping
+        self.resolver_map[TXN.MemCheck] = self.__handle_memcheck
+        self.resolver_map[TXN.Ping] = self.__handle_ping
+        self.resolver_map[TXN.GetPingSites] = self.__get_ping_sites
+        self.resolver_map[TXN.Goodbye] = self.__handle_goodbye
+        self.resolver_map[TXN.Suicide] = self.__handle_suicide
+
+    def _get_resolver(self, txn):
+        return self.resolver_map[TXN(txn)]
+
+    def _get_creator(self, txn):
+        return self.creator_map[TXN(txn)]
+
+    async def __handle_hello(self, data):
+        """Initial packet sent by client, used to determine client type, and other connection details"""
+
+        client_data = {
+            "clientString": data.Get("clientString"),
+            "clientPlatform": data.Get("clientPlatform"),
+            "clientVersion": data.Get("clientVersion"),
+            "clientType": data.Get("clientType"),
+            "sku": data.Get("sku"),
+            "locale": data.Get("locale"),
+            "SDKVersion": data.Get("SDKVersion"),
+            "protocolVersion": data.Get("protocolVersion"),
+            "fragmentSize": data.Get("fragmentSize"),
+        }
+
+        await self.connection.initialize_connection(client_data)
+
+        domainPartition = {
+            "domain": "eagames",
+            "subDomain": "BFBC2",
+        }
+
+        theater_ip, theater_port = (None, None)
+
+        if self.connection.clientType == ClientType.CLIENT:
+            theater_ip = THEATER_CLIENT_IP
+            theater_port = THEATER_CLIENT_PORT
+        else:
+            theater_ip = THEATER_SERVER_IP
+            theater_port = THEATER_SERVER_PORT
+
+        response = Packet()
+        response.Set(
+            "activityTimeoutSecs", 0
+        )  # Client sets this to 200 (3.33 minutes) if set to 0
+        response.Set("curTime", datetime.now())
+        response.Set(
+            "domainPartition", domainPartition
+        )  # Game doesn't seem to care about this, but it's sent by the original server
+        response.Set("messengerIp", MESSENGER_IP)
+        response.Set("messengerPort", MESSENGER_PORT)
+        response.Set("theaterIp", theater_ip)
+        response.Set("theaterPort", theater_port)
+
+        # Client also supports "addressRemapping" value here, but it's never sent by the original server
+        # response.Set("addressRemapping", '\0')
+        # If not set, client will set this value to NULL internally (just like above)
+        # No clue what this value is used for
+
+        return response
+
+    async def __create_memcheck(self, data):
+        """Create a MemCheck packet"""
+
+        response = Packet()
+
+        # Never saw memcheck containing something (original server also always sends empty memcheck)
+        # But memcheck object should look like this
+        # {
+        #    "addr": integer (4 bytes),
+        #    "len": integer (4 bytes),
+        # }
+        # Never seen that in the wild, and above is just a guess based on my client reverse engineering
+        # No idea how this affects the client, but original server never sends it either so we send empty array here
+        response.Set("memcheck", [])
+        response.Set("type", 0)
+        response.Set("salt", "".join(random.choice(string.digits) for _ in range(10)))
+
+        return response
+
+    async def __create_ping(self, data):
+        """Create a Ping packet"""
+
+        response = Packet()
+        return response
+
+    async def __handle_memcheck(self, data):
+        """Handle a MemCheck packet"""
+
+        # Ignore
+        return TransactionSkip()
+
+    async def __handle_ping(self, data):
+        """Handle a Ping packet"""
+
+        # Ignore
+        return TransactionSkip()
+
+    async def __get_ping_sites(self, data):
+        """Get a list of ping sites"""
+
+        # Original server always sends 4 ping sites
+        # {
+        #    "name": "nrt",
+        #    "type": 0,
+        #    "addr": "109.200.220.1"
+        # }
+        # {
+        #    "name": "gva",
+        #    "type": 0,
+        #    "addr": "159.153.72.181"
+        # }
+        # {
+        #    "name": "sjc",
+        #    "type": 0,
+        #    "addr": "159.153.70.181"
+        # }
+        # {
+        #    "name": "iad",
+        #    "type": 0,
+        #    "addr": "159.153.93.213"
+        # }
+
+        # Because we're only emulating the master server, send the same ping sites, but with localhost address instead (not sure how this affects the client)
+        ping_sites = [
+            {"name": name, "type": 0, "addr": "127.0.0.1"}
+            for name in ["nrt", "gva", "sjc", "iad"]
+        ]
+
+        response = Packet()
+        response.Set("pingSite", ping_sites)
+        response.Set("minPingSitesToPing", 0)
+
+        return response
+
+    async def __handle_goodbye(self, data):
+        """Handle a Goodbye packet (sent by client when disconnecting)"""
+
+        # Just log why client disconnected
+        self.connection.logger.info(
+            f"Client disconnected (Reason: {data.Get('reason')}, Message: {data.Get('message')})"
+        )
+        return TransactionSkip()
+
+    async def __handle_suicide(self, data):
+        """Handle Suicide packet"""
+
+        # This packet only contains TXN, no idea what it's supposed to do
+        # It seems that it's client who would send this kind of transaction
+        # But I never managed to make game send this packet
+        # So ignore this packet
+
+        return TransactionSkip()
