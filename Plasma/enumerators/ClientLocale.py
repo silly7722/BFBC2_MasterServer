e45c4ec5b929c0583652a00bf3daedee6995e333
Marek Grzyb <grzybdev@gmail.com>
2022-11-24 19:06:19 +0100

    Reorganized ClientLocales to match game order

--- Plasma/enumerators/ClientLocale.py
+++ Plasma/enumerators/ClientLocale.py
@@ -4,9 +4,9 @@ from enum import Enum
 class ClientLocale(Enum):
     English = "en_US"
     French = "fr_FR"
-    Spanish = "es"
     German = "de"
+    Spanish = "es"
     Italian = "it"
-    Polish = "pl"
-    Russian = "ru"
     Japanese = "ja"
+    Russian = "ru"
+    Polish = "pl"
