bd5230ee737222ae3bc0762842041fc33a2caf2d
Marek Grzyb <grzybdev@gmail.com>
2022-11-25 20:27:55 +0100

    Added activate_game method to EntitlementManager

--- Plasma/managers.py
+++ Plasma/managers.py
@@ -1,8 +1,9 @@
-from datetime import datetime
-
 from asgiref.sync import sync_to_async
 from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
+from django.utils import timezone
+
+from Plasma.enumerators.ActivationResult import ActivationResult
 
 
 class UserManager(BaseUserManager):
@@ -44,13 +45,59 @@ class UserManager(BaseUserManager):
 
 
 class EntitlementManager(models.Manager):
-    @sync_to_async
-    def is_entitled_for_game(self, user, game_id):
-        return self.filter(
+    async def is_entitled_for_game(self, user, game_id):
+        filtered_entitlement = await sync_to_async(self.filter)(
             account=user,
             tag=game_id,
-            grantDate__gte=datetime.now(),
-            terminationDate__lte=datetime.now(),
-            status="ACTIVE",
+            grantDate__lt=timezone.now(),
             isGameEntitlement=True,
-        ).exists()
+        )
+
+        never_expires = await sync_to_async(filtered_entitlement.filter)(
+            terminationDate=None
+        )
+
+        if await sync_to_async(never_expires.exists)():
+            return True
+        else:
+            timed_entitlements = await sync_to_async(filtered_entitlement.filter)(
+                terminationDate__gt=timezone.now()
+            )
+
+            return await sync_to_async(timed_entitlements.exists)()
+
+    async def activate_game(self, user, key):
+        from Plasma.models import SerialKey
+
+        valid_keys = await sync_to_async(SerialKey.objects.filter)(
+            key=key, is_game_key=True
+        )
+        game_targets = []
+
+        if await sync_to_async(valid_keys.exists)():
+            key = await sync_to_async(SerialKey.objects.get)(key=key)
+            game_targets = key.targets.split(";")
+        else:
+            return ActivationResult.INVALID_KEY
+
+        if key.is_used:
+            return ActivationResult.ALREADY_USED
+
+        for game in game_targets:
+            if await self.is_entitled_for_game(user, game):
+                continue
+
+            await sync_to_async(self.create)(
+                account=user,
+                tag=game,
+                grantDate=timezone.now(),
+                isGameEntitlement=True,
+            )
+
+        if not key.is_permanent:
+            key.is_used = True
+            key.used_at = timezone.now()
+            key.used_by = user
+            await sync_to_async(key.save)()
+
+        return ActivationResult.SUCCESS
