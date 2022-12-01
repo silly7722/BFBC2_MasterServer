1ee9ff053ddd5143b36d35f094b7bc7f822596ae
Marek Grzyb <grzybdev@gmail.com>
2022-12-01 21:52:57 +0100

    Added support for AddAssocations transactions

--- Plasma/services/association.py
+++ Plasma/services/association.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
+from BFBC2_MasterServer.packet import Packet
 from BFBC2_MasterServer.service import Service
+from channels.auth import database_sync_to_async, get_user
+from Plasma.enumerators.ListFullBehavior import ListFullBehavior
+from Plasma.error import TransactionError
+from Plasma.models import Assocation
 
 
 class TXN(Enum):
@@ -15,8 +20,100 @@ class AssociationService(Service):
     def __init__(self, connection) -> None:
         super().__init__(connection)
 
+        self.resolver_map[TXN.AddAssociations] = self.__handle_add_associations
+
     def _get_resolver(self, txn):
         return self.resolver_map[TXN(txn)]
 
     def _get_creator(self, txn):
         return self.creator_map[TXN(txn)]
+
+    def __get_assocation_type(self, assoType):
+        match assoType:
+            case "PlasmaMute":
+                return Assocation.type.MUTE
+            case "PlasmaBlock":
+                return Assocation.type.BLOCK
+            case "PlasmaFriends":
+                return Assocation.type.FRIENDS
+            case "PlasmaRecentPlayers":
+                return Assocation.type.RECENT_PLAYERS
+            case _:
+                return None
+
+    async def __handle_add_associations(self, data):
+        """Add associations between two objects."""
+
+        domainPartition = data.Get("domainPartition")
+        listFullBehavior = data.Get("listFullBehavior")
+
+        if not listFullBehavior:
+            return TransactionError(TransactionError.Code.PARAMETERS_ERROR)
+
+        listFullBehavior = ListFullBehavior(listFullBehavior)
+
+        assoType = self.__get_assocation_type(data.Get("type"))
+
+        if not assoType:
+            return TransactionError(TransactionError.Code.PARAMETERS_ERROR)
+
+        addRequests = data.Get("addRequests")
+
+        assoUsr = await Assocation.objects.get_user_assocations(
+            self.connection.loggedPersona, assoType
+        )
+        maxAssocations = 20 if assoType != Assocation.type.RECENT_PLAYERS else 100
+
+        result = []
+
+        for addRequest in addRequests:
+            outcome = 0
+
+            if listFullBehavior == ListFullBehavior.ReturnError:
+                if len(assoUsr.members) + 1 > maxAssocations:
+                    outcome = 23005
+            elif listFullBehavior == ListFullBehavior.RollLeastRecentlyModified:
+                if len(assoUsr.members) + 1 > maxAssocations:
+                    # Roll least recently modified
+                    members = await database_sync_to_async(assoUsr.members.all)()
+
+                    # Order by oldest first
+                    members = sorted(members, key=lambda member: member.lastModified)
+                    oldestMember = members[0]
+
+                    # Remove oldest member
+                    await database_sync_to_async(assoUsr.members.remove)(oldestMember)
+
+            # Add new member
+            member = await Assocation.objects.add_assocation(
+                assoUsr, addRequest["member"]["id"]
+            )
+
+            if not member:
+                return TransactionError(
+                    TransactionError.Code.TRANSACTION_DATA_NOT_FOUND
+                )
+
+            owner = {
+                "id": self.connection.loggedPersona.id,
+                "name": self.connection.loggedPersona.name,
+                "type": 1,
+            }
+
+            resultFinal = {
+                "member": member,
+                "owner": owner,
+                "mutual": 0 if assoType == Assocation.type.RECENT_PLAYERS else 1,
+                "outcome": outcome,
+                "listSize": maxAssocations,
+            }
+
+            result.append(resultFinal)
+
+        response = Packet()
+        response.Set("domainPartition", domainPartition)
+        response.Set("maxListSize", maxAssocations)
+        response.Set("results", result)
+        response.Set("type", data.Get("type"))
+
+        return response
