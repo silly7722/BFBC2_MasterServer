bd11abb9edae73502c1474303d690edd989ca455
Marek Grzyb <grzybdev@gmail.com>
2022-12-07 21:24:26 +0100

    Use real client ip in echo packet

--- Theater/transactions/echo.py
+++ Theater/transactions/echo.py
@@ -4,11 +4,11 @@ from BFBC2_MasterServer.packet import Packet
 def echo(connection, message):
     """Handle echo transaction"""
 
-    ip, port = connection.scope["client"]
+    _, port = connection.scope["client"]
 
     response = Packet()
     response.Set("TXN", message.service)
-    response.Set("IP", ip)
+    response.Set("IP", connection.ip)
     response.Set("PORT", port)
     response.Set("ERR", 0)
     response.Set("TYPE", 1)
