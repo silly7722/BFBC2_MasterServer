8ad1131e5b91914d23c727e3168277078ca3c097
Marek Grzyb <grzybdev@gmail.com>
2022-12-07 21:24:53 +0100

    Added Lobby and Game models to admin site

--- Theater/admin.py
+++ Theater/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from Theater.models import Game, Lobby
+
+
 # Register your models here.
+class LobbyAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "locale",
+        "maxGames",
+    ]
+    list_filter = (
+        "name",
+        "locale",
+    )
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ["id", "lobby", "name"]
+    list_filter = ("lobby", "name")
+
+
+admin.site.register(Lobby, LobbyAdmin)
+admin.site.register(Game, GameAdmin)
