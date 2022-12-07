e83d8bdb4c6d42a5e4326e2061262c0616c72aee
Marek Grzyb <grzybdev@gmail.com>
2022-12-07 21:21:55 +0100

    Changed ClientPlatform type to TextChoices

--- Plasma/enumerators/ClientPlatform.py
+++ Plasma/enumerators/ClientPlatform.py
@@ -1,7 +1,7 @@
-from enum import Enum
+from django.db.models import TextChoices
 
 
-class ClientPlatform(Enum):
+class ClientPlatform(TextChoices):
     XBOX360 = "xenon"
     PS3 = "ps3"
     PC = "PC"
